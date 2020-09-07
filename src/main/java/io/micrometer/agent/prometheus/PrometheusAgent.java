60ffd65fcbe60f32e908f734253388de3c0e27c3
Jonathan Schneider <jkschneider@gmail.com>
2020-09-07 14:17:20 -0700

    Shade

--- src/main/java/io/micrometer/agent/prometheus/PrometheusAgent.java
+++ src/main/java/io/micrometer/agent/prometheus/PrometheusAgent.java
@@ -1,15 +1,11 @@
-package io.micrometer.prometheus.agent;
+package io.micrometer.agent.prometheus;
 
 import io.javalin.Javalin;
-import io.javalin.plugin.metrics.MicrometerPlugin;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmHeapPressureMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.system.FileDescriptorMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
-import io.micrometer.core.instrument.binder.tomcat.TomcatMetrics;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.exporter.common.TextFormat;
